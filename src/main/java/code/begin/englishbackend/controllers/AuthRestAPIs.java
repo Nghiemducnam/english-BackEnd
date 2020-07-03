184642a0167be9e27a1da4a7493fd6bd72460e96
Nghiemducnam <nghiemducnam0312@gmail.com>
2020-07-03 23:27:28 +0700

    update controller

--- src/main/java/code/begin/englishbackend/controllers/AuthRestAPIs.java
+++ src/main/java/code/begin/englishbackend/controllers/AuthRestAPIs.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashSet;
 import java.util.Set;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthRestAPIs {
